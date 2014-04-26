c9c6f30bcd58032db50e22ed5071f3608ac01329
Chris Alfano <calfano@codeforamerica.org>
2014-04-26 14:29:10 -0400

    filter updates page by project

--- php-classes/Laddr/ProjectUpdatesRequestHandler.php
+++ php-classes/Laddr/ProjectUpdatesRequestHandler.php
@@ -6,8 +6,18 @@ class ProjectUpdatesRequestHandler extends \RecordsRequestHandler
 {
     static public $recordClass = 'Laddr\ProjectUpdate';
     static public $accountLevelBrowse = false;
-    static public $accountLevelWrite = 'User'; // TODO: implement custom write checker that authenticats only project members
+    static public $accountLevelWrite = 'User';
     static public $browseOrder = array('ID' => 'DESC');
+
+    static public function handleBrowseRequest($options = array(), $conditions = array(), $responseID = null, $responseData = array())
+    {
+        if (!empty($_GET['ProjectID']) && ctype_digit($_GET['ProjectID']) && ($Project = Project::getByID($_GET['ProjectID']))) {
+            $conditions['ProjectID'] = $Project->ID;
+            $responseData['Project'] = $Project;
+        }
+        
+        return parent::handleBrowseRequest($options, $conditions, $responseID, $responseData);
+    }
 
     static public function checkWriteAccess(\ActiveRecord $ProjectUpdate, $suppressLogin = false)
     {
