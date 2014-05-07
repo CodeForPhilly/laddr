98534c0681cd527268292ab4d41f084dd0126c7c
Chris Alfano <calfano@codeforamerica.org>
2014-05-06 20:56:49 -0400

    trim descriptions for projects.csv output

--- site-root/projects.csv.php
+++ site-root/projects.csv.php
@@ -3,9 +3,10 @@
 RequestHandler::$responseMode = 'csv';
 RequestHandler::respond('projects', array(
     'data' => array_map(function($Project) {
+        preg_match('/^\s*[^*#]\s*\w.*/m', $Project->README, $matches);
         return array(
             'name' => $Project->Title
-            ,'description' => $Project->README
+            ,'description' => trim($matches[0])
             ,'link_url' => $Project->UsersUrl
             ,'code_url' => $Project->DevelopersUrl
             ,'type' => ''
