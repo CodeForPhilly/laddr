a1100774ec4be38af3fe6f4556ceb37a9a11b724
Chris Alfano <calfano@codeforamerica.org>
2014-08-17 12:25:15 -0400

    moved default slogan to config file with gettext wrapper

---
+++ php-config/Laddr.config.php
@@ -0,0 +1,5 @@
+<?php
+
+#Laddr::$siteName = _('Code for Philly');
+#Laddr::$siteAbbr = _('CfP');
+Laddr::$siteSlogan = _('Making our community a better place to live, work, and play through technology.');
