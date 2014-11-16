606a2ef2c1076397743e2038d618dcaf04f250a7
Chris Alfano <calfano@codeforamerica.org>
2014-11-16 15:46:48 -0500

    new config for filtering meetup events

--- php-classes/RemoteSystems/Meetup.php
+++ php-classes/RemoteSystems/Meetup.php
@@ -9,13 +9,14 @@ class Meetup
     public static $groupUrl;
     public static $signedEventsUrl;
     public static $defaultCacheTime = 1800;
-    
+    public static $eventsFilter;
+
     public static function getEventUrl($meetupID)
     {
         if (!static::$groupUrl) {
             return '#';
         }
-        
+
         return sprintf('http://www.meetup.com/%s/events/%u/', static::$groupUrl, $meetupID);
     }
 
@@ -33,6 +34,16 @@ class Meetup
 
         if (!$data = Cache::fetch($cacheKey)) {
             $data = json_decode(file_get_contents(static::$signedEventsUrl), true);
+
+            if (static::$eventsFilter) {
+                if (is_string(static::$eventsFilter)) {
+                    $data['results'] = array_filter($data['results'], function($event) {
+                        return preg_match(static::$eventsFilter, $event['name']);
+                    });
+                } elseif (is_callable(static::$eventsFilter)) {
+                    $data['results'] = array_filter($data['results'], static::$eventsFilter);
+                }
+            }
 
             if (!empty($data['results'])) {
                 Cache::store($cacheKey, $data, $ttl);
