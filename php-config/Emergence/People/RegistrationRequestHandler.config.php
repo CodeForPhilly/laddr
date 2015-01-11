c1a029e46759d4fcafd16fac999bfdb6e3d669e0
Chris Alfano <calfano@codeforamerica.org>
2015-01-11 15:44:23 -0500

    better comments in sample registration hook

--- php-config/Emergence/People/RegistrationRequestHandler.config.php
+++ php-config/Emergence/People/RegistrationRequestHandler.config.php
@@ -3,11 +3,11 @@
 namespace Emergence\People;
 
 #RegistrationRequestHandler::$onRegisterComplete = function(User $User, array $requestData) {
-#    // this hook to run after a new user acconut has been registered (the user is waiting though keep things moving!)
+#    // this hook is run after a new user acconut has been registered (the user is waiting though so keep things moving!)
 #
-#    // add new user to a MailChimp list
+#    // add new user to a MailChimp list (see http://forum.laddr.io/t/mailchimp-integration/26)
 #    \RemoteSystems\MailChimp::call('lists/subscribe', [
-#        'id' => 'YOUR_LIST_ID_HERE', // find your list's id on the MailChimp website under Setting > List name & defaults
+#        'id' => 'YOUR_LIST_ID_HERE',
 #        'email' => [
 #            'email' => $User->Email
 #        ],
