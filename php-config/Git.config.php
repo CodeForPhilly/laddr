e95584bdd3d231626b3f970d79a064d54deac5b7
Chris Alfano <chris@jarv.us>
2015-11-17 19:55:08 -0500

    Add ReCaptcha repo to stock Git.config.php

--- php-config/Git.config.php
+++ php-config/Git.config.php
@@ -1,18 +1,29 @@
 <?php
 
 Git::$repositories['laddr'] = [
-    'remote' => 'https://github.com/CfABrigadePhiladelphia/laddr.git'
-    ,'originBranch' => 'releases/v2'
-    ,'workingBranch' => 'releases/v2'
-    ,'localOnly' => true
-    ,'trees' => [
+    'remote' => 'https://github.com/CfABrigadePhiladelphia/laddr.git',
+    'originBranch' => 'releases/v2',
+    'workingBranch' => 'releases/v2',
+    'localOnly' => true,
+    'trees' => [
         'html-templates',
         'locales',
-        'php-classes',
+        'php-classes' => [
+            'exclude' => '#^/ReCaptcha/#' // exclude ReCaptcha library pulled from google repo
+        ],
         'php-config' => [
             'exclude' => '#^/Git\\.config\\.php$#' // don't sync this file
         ],
         'php-migrations',
         'site-root'
+    ]
+];
+
+Git::$repositories['ReCaptcha'] = [
+    'remote' => 'https://github.com/google/recaptcha.git',
+    'originBranch' => 'master',
+    'workingBranch' => 'master',
+    'trees' => [
+        'php-classes/ReCaptcha' => 'src/ReCaptcha'
     ]
 ];
