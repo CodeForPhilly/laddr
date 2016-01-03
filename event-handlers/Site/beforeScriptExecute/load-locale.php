2afd6886b6bd62111203d5a186d3d3ea8d2ee0a8
Chris Alfano <chris@jarv.us>
2016-01-02 20:25:46 -0500

    Remove Site.config.php override in favor of events and composite configs

---
+++ event-handlers/Site/beforeScriptExecute/load-locale.php
@@ -0,0 +1,3 @@
+<?php
+
+Emergence\Locale::loadRequestedLocale();
---
+++ php-config/VersionedRecord.config.d/track-modified.php
@@ -0,0 +1,4 @@
+<?php
+
+// auto-written by php-migrations/Emergence/VersionedRecord/20151230000000_enable_track_modified.php
+VersionedRecord::$trackModified = true;
