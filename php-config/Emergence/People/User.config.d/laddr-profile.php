d55bd611e869154acef638c9c3c40fba1c1e9dc2
Chris Alfano <chris@jarv.us>
2016-01-03 17:04:52 -0500

    Add tech/topic tag relationships to Users

--- php-config/Emergence/People/User.config.d/laddr-profile.php
+++ php-config/Emergence/People/User.config.d/laddr-profile.php
@@ -15,6 +15,28 @@ User::$relationships['Tags'] = [
     'conditions' => ['Link.ContextClass = "Emergence\\\\People\\\\Person"']
 ];
 
+User::$relationships['TopicTags'] = [
+    'type' => 'many-many',
+    'class' => \Tag::class,
+    'linkClass' => \TagItem::class,
+    'linkLocal' => 'ContextID',
+    'conditions' => [
+        'Link.ContextClass = "Emergence\\\\People\\\\Person"',
+        'Related.Handle LIKE "topic.%"'
+    ]
+];
+
+User::$relationships['TechTags'] = [
+    'type' => 'many-many',
+    'class' => \Tag::class,
+    'linkClass' => \TagItem::class,
+    'linkLocal' => 'ContextID',
+    'conditions' => [
+        'Link.ContextClass = "Emergence\\\\People\\\\Person"',
+        'Related.Handle LIKE "tech.%"'
+    ]
+];
+
 User::$relationships['Comments'] = [
     'type' => 'context-children',
     'class' => \Comment::class,
