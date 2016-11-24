7572933ba84ae154707bfff2180e1ef853218e35
Chris Alfano <chris@jarv.us>
2016-11-24 13:43:13 -0500

    Provide default implementation of /chat that uses Laddr::$chatLinker

--- site-root/chat.php
+++ site-root/chat.php
@@ -1,13 +1,8 @@
 <?php
 
-// TODO: delete/comment this line and use this file to implement a gateway to your group's chat system
-return RequestHandler::throwError('The administrator of this site has not yet implemented a redirect to your chat provider');
+if (Laddr::$chatLinker) {
+    $url = call_user_func(Laddr::$chatLinker, !empty($_GET['channel']) ? $_GET['channel'] : null);
+    Site::redirect($url);
+}
 
-// $_GET['channel'] may be provided to indicate a specific channel to redirect to
-
-// EXAMPLE 1: Redirect to Slack with authentication provided via SAML Single Sign On
-//$GLOBALS['Session']->requireAuthentication();
-//Site::redirect('https://codeforphilly.slack.com/sso/saml/start?redir=%2Fmessages%2F'.urlencode(!empty($_GET['channel']) ? $_GET['channel'] : 'general').'%2F');
-
-// EXAMPLE 2: Redirect to Slack without SSO
-//Site::redirect('https://codeforphilly.slack.com/messages/'.urlencode(!empty($_GET['channel']) ? $_GET['channel'] : 'general').'/');
+RequestHandler::throwError('The administrator of this site has not yet configured Laddr:$chatLinker');
