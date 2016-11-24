de6fd3480faf0d96aca38561462b1ed1a0c1d917
Chris Alfano <chris@jarv.us>
2016-11-24 13:42:48 -0500

    Provide example of direct Slack linking for chatLinker

--- php-config/Laddr.config.d/chat.php
+++ php-config/Laddr.config.d/chat.php
@@ -2,12 +2,6 @@
 
 /*
 Laddr::$chatLinker = function($channel = null) {
-    $url = '/chat';
-
-    if ($channel) {
-        $url .= '?channel=' . urlencode($channel);
-    }
-
-    return $url;
+    return 'https://mySlackOrg.slack.com/messages/'.urlencode($channel ?: 'general').'/';
 };
 */
