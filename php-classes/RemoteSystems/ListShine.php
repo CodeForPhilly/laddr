aeadb4373958d7468773c03ffcb4b02d5c3c224e
MPorcic <porcicmario@gmail.com>
2017-03-24 20:16:40 +0100

    Update ListShine.php

--- php-classes/RemoteSystems/ListShine.php
+++ php-classes/RemoteSystems/ListShine.php
@@ -7,10 +7,10 @@ class ListShine
 {
     public static $api_key;
     public static $url = 'http://send.listshine.com/api/v1/';
-    public static $listshine_connection;
+    public $listshine_connection;
 
     public static function subscribe($email, $list_uuid, $value_array){
-         $random = new \ListShine\ListShineApi(static::$api_key, static::$url);
-         $random->subscribeUser($email, $list_uuid, $value_array);
+         $listshine_connection = new \ListShine\ListShineApi(static::$api_key, static::$url);
+         $listshine_connection->subscribeUser($email, $list_uuid, $value_array);
     }
 }
