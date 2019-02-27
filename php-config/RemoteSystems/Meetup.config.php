46a90ba020b94f40e7c6e2f6f41c82045fd40982
Marcel Kooi <1marcelkooi@gmail.com>
2019-02-26 20:39:38 -0500

    Remove meetup url

--- php-config/RemoteSystems/Meetup.config.php
+++ php-config/RemoteSystems/Meetup.config.php
@@ -1,4 +1,4 @@
 <?php
 
-RemoteSystems\Meetup::$groupUrl = 'Code-for-Philly';
-RemoteSystems\Meetup::$signedEventsUrl = 'https://api.meetup.com/2/events?offset=0&format=json&limited_events=False&group_urlname=Code-for-Philly&photo-host=public&page=200&fields=&order=time&status=upcoming&desc=false&sig_id=14053930&sig=25e974c166094e8e34ffd42f46388f8f7619c076';
+#RemoteSystems\Meetup::$groupUrl = 'YOU_MEETUP_GROUP_URL'; // http://www.meetup.com/YOU_MEETUP_GROUP_URL
+#RemoteSystems\Meetup::$signedEventsUrl = 'http://YOUR_SIGNED_EVENTS_FEED_URL_HERE';
