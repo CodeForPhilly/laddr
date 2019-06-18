c4c3e4a1fe1e3748ccc622a5a3b9d092beb2127e
Chris Alfano <calfano@codeforamerica.org>
2019-06-18 19:53:14 -0400

    fix: enable a default site name

--- php-config/Laddr.config.d/branding.php
+++ php-config/Laddr.config.d/branding.php
@@ -1,5 +1,5 @@
 <?php
 
-#Laddr::$siteName = _('Code for Philly');
+Laddr::$siteName = _('Brigade');
 #Laddr::$siteAbbr = _('CfP');
 Laddr::$siteSlogan = _('Making our community a better place to live, work, and play through technology.');
