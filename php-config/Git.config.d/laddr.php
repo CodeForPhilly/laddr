a91162a103a129ce004bad07b47758cde9e79115
Chris Alfano <chris@jarv.us>
2019-08-27 15:29:46 +0000

    chore: update legacy git config to pull vfs-site holobranch

--- php-config/Git.config.d/laddr.php
+++ php-config/Git.config.d/laddr.php
@@ -2,9 +2,22 @@
 
 Git::$repositories['laddr'] = [
     'remote' => 'https://github.com/CodeForPhilly/laddr.git',
-    'originBranch' => 'emergence/layer',
-    'workingBranch' => 'emergence/layer',
+    'originBranch' => 'emergence/vfs-site/v3',
+    'workingBranch' => 'emergence/vfs-site/v3',
     'trees' => [
-        '.'
+        'api-docs',
+        'content-blocks',
+        'data-exporters',
+        'dwoo-plugins',
+        'event-handlers',
+        'html-templates',
+        'php-classes',
+        'php-config',
+        'php-migrations',
+        'phpunit-tests',
+        'site-root',
+        'site-tasks',
+        'webapp-builds',
+        'webapp-plugin-builds'
     ]
 ];
