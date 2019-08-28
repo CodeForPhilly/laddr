4c046fe109b29c9e934f16683daeb603c27e997f
Chris Alfano <chris@jarv.us>
2019-08-28 02:27:06 +0000

    fix: patch ical parser for VFS use

---
+++ php-classes/om/Emergence/IcalParser.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace om\Emergence;
+
+use Site;
+
+/**
+ * Replace constructor to load table from VFS instead of local path
+ */
+class IcalParser extends \om\IcalParser
+{
+    public function __construct()
+    {
+        $node = Site::resolvePath('php-classes/om/WindowsTimezones.php');
+        $this->windowsTimezones = require($node->RealPath); // load Windows timezones from separate file
+    }
+}
